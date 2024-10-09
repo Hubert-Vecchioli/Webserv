d21bda3ec922b2ad8dd5390bbe7701885ddc7209
Julie Blaye <jblaye@bess-f4r4s11.clusters.42paris.fr>
2024-10-09 18:49:41 +0200

    [MOD] httprequest file for new attributes and members

--- include/HttpRequest.hpp
+++ include/HttpRequest.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   HttpRequest.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hvecchio <hvecchio@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 16:54:18 by hvecchio          #+#    #+#             */
-/*   Updated: 2024/10/08 07:54:46 by hvecchio         ###   ########.fr       */
+/*   Updated: 2024/10/09 18:33:42 by jblaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,44 @@
 
 #include "webserv.hpp"
 
+typedef enum e_method {
+	GET,
+	POST,
+	DELETE,
+	ERROR
+}				t_method;
+
+typedef enum e_connection {
+	KEEP_ALIVE,
+	CLOSE
+}				t_connection;
+
 class HttpRequest
 {
 	private:
 		Client*								_client;
 		HttpResponse*						_response;
+		t_method							_method;
+		bool								_http1;
+		std::string							_queryString;
+		std::string							_requestURI;
+		std::string							_HTTPVersion;
+		std::string							_host;
+		t_connection						_connection;
+		std::string							_content_type;
+		size_t								_content_len;
+		std::string							_content_body;
+		std::string							_accept;
 
 	public:
-		HttpRequest(void);
+		HttpRequest(char *request);
 		HttpRequest(HttpRequest const & rhs);
 		HttpRequest &operator=(HttpRequest const & rhs);
 		~HttpRequest(void);
+
+		parseHeader(char *request);
+		parseRequestLine(char *request);
+    	parseBody(char *request);
 
 		HttpResponse* getResponse(void) {return _response};
 		static HttpRequest* findInstanceWithFD(std::vector<HttpRequest>& vector, int fd);
