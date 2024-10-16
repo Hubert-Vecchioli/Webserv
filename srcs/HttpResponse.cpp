40580c02a7cf5cb45427bb79cad93fb94c96558c
Julie Blaye <jblaye@made-f0Br8s13.clusters.42paris.fr>
2024-10-16 18:07:45 +0200

    [ADD] functions to generate the response in case of error

--- srcs/HttpResponse.cpp
+++ srcs/HttpResponse.cpp
@@ -6,7 +6,7 @@
 /*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 16:56:19 by hvecchio          #+#    #+#             */
-/*   Updated: 2024/10/16 14:58:04 by jblaye           ###   ########.fr       */
+/*   Updated: 2024/10/16 18:05:17 by jblaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,9 +71,18 @@ Allow: GET, POST
 
 void HttpResponse::_generateGETResponse(HttpRequest & request)
 {
+	std::stringstream ss;
 	if (request.getHTTP == false)
 		throw ServerError(505);
-	if ()
+	_reponseContent = "HTTP/1.1 ";
+	try {
+		ServerBlock server_block = _fetchServerBlock(request);
+		try {
+			LocationBlock location_block = _fetchLocationBlock(request, server_block);
+		}
+	}
+	catch (HttpResponse::ClientError & e) {
+	}
 }
 
 //Assuming POST is only to upload files
@@ -149,12 +158,70 @@ void HttpResponse::_generateDELResponse(void)
 	this->_reponseContent = "HTTP/1.1 204 No Content\r\n";
 }
 
+void HttpResponse::_generateErrorResponse(int errorCode, char *errorMessage, ServerBlock & server_block) {
+	std::stringstream ss;
+	
+	ss << "HTTP/1.1 " << errorCode << " " << errorMessage << "\r\n";
+	ss << _displayTimeStamp() << "\r\n";
+	ss << "Content-Type: text/html\r\n";
+	if (!server_block)
+		return _generateGenericErrorResponse(errorCode, errorMessage);
+	std::string error_page = server_block.getErrorPages()[errorCode];
+	if (!error_page)
+		return _generateGenericErrorResponse(errorCode, errorMessage);
+	struct stat stats;
+	if (stat(&stats) == -1)
+		return _generateGenericErrorResponse(errorCode, errorMessage);
+	size_t size = stats.st_size;
+	ss << "Content-Size: " << size << "\r\n";
+	ss << "\r\n";
+	
+	std::ifstream file(error_page.c_str());
+	if (!file.is_open())
+		return _generateGenericErrorResponse(errorCode, errorMessage);
+	std::string line;
+	while (getline(file, line))
+		ss << line << "\r\n";
+	file.close();
+	_responseContent = ss.str();		
+}
+
+void HttpResponse::_generateGenericErrorResponse(int errorCode, char *errorMessage) {
+	std::stringstream ss;
+	std::stringstream body;
+
+	body << "<html><head><title>";
+	body << errorCode << " " << errorMessage;
+	body << "</title></head><body><h1>";
+	body << errorCode << " " << errorMessage;
+	body << "</h1></body></html>";
+	
+	ss << "HTTP/1.1 " << errorCode << " " << errorMessage;
+	ss << _displayTimeStamp()<< "\r\n";
+	ss << "Content-Type: text/html\r\n";
+	ss << "Content-Lenght: " << body.gcount() << "\r\n";
+	ss << "\r\n";
+	ss << body;
+
+	_responseContent = ss.str();
+}
+
+std::stringstream	HttpResponse::_displayTimeStamp(void) {
+	std::stringstream ss;
+	struct tm *gmt = gmtime(&_lastActionTimeStamp);
+	char buffer[100];
+	
+	strftime(buffer, sizeof(buffer), "%a, %d %b %Y %H:%M:%S GMT", gmt);
+
+	ss << "Date: " << buffer;
+    return ss;
+}
 
 std::string HttpResponse::getResponseContent(void)
 {
 	if (this->_reponseContent.empty())
 		this->_generateResponseContent();
-	return (this->_reponseContent);
+	return (this->_reponseContent());
 }
 
 const char *HttpResponse::ServerError::what() const throw() {
