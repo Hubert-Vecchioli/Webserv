9456c55606e47c956e5131e2fcf21c6d208f743d
Emma Besnoin <ebesnoin@student.42.fr>
2024-10-17 17:35:51 +0200

    [ADD] starting CGI

---
+++ include/CgiHandler.hpp
@@ -0,0 +1,28 @@
+#ifndef CGIHANDLER_HPP
+# define CGIHANDLER_HPP
+
+#include "webserv.hpp"
+
+class CgiHandler
+{
+	public:
+		CgiHandler(HttpRequest const &request);
+		CgiHandler(CgiHandler const & rhs);
+		CgiHandler &operator=(CgiHandler const & rhs);
+		~CgiHandler(void);
+	
+	private:
+		std::map<std::string, std::string> _env;
+							//"AUTH_TYPE" | "CONTENT_LENGTH" |
+                        	//"CONTENT_TYPE" | "GATEWAY_INTERFACE" |
+                        	//"PATH_INFO" | "PATH_TRANSLATED" |
+                       	    //"QUERY_STRING" | "REMOTE_ADDR" |
+                       	    //"REMOTE_HOST" | "REMOTE_IDENT" |
+                       	    //"REMOTE_USER" | "REQUEST_METHOD" |
+                       	    //"SCRIPT_NAME" | "SERVER_NAME" |
+                       	    //"SERVER_PORT" | "SERVER_PROTOCOL" |
+                       	    //"SERVER_SOFTWARE"
+
+}
+
+#endif //CGIHANDLER_HPP
