eb7035a49bf1d2ddaf4079cc55d97c418823de68
Emma Besnoin <ebesnoin@student.42.fr>
2024-10-17 17:35:51 +0200

    [ADD] starting CGI

---
+++ srcs/CgiHandler.cpp
@@ -0,0 +1,21 @@
+#include "CgiHandler.hpp"
+
+CgiHandler::CgiHandler(HttpRequest const &request, ) {
+	_env["AUTH_TYPE"] = "";
+	_env["CONTENT_LENGTH"] = std::to_string(request.getContentLen());
+	_env["CONTENT_TYPE"] = request.getContentType();
+	_env["GATEWAY_INTERFACE"] = "CGI/1.1";
+	_env["PATH_INFO"] = "";
+	_env["PATH_TRANSLATED"] = "";
+	_env["QUERY_STRING"] = request.getQueryString();
+	_env["REMOTE_ADDR"] = "";
+	_env["REMOTE_HOST"] = "";
+	_env["REMOTE_IDENT"] = "";
+	_env["REMOTE_USER"] = "";
+	_env["REQUEST_METHOD"] = request.getMethod();
+	_env["SCRIPT_NAME"] = "";
+	_env["SERVER_NAME"] = "";
+	_env["SERVER_PORT"] = "";
+	_env["SERVER_PROTOCOL"] = "HTTP/1.1";
+	_env["SERVER_SOFTWARE"] = "webserv";
+}
