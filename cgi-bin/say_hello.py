d01e75fce3276c634485c099cfae4b604c9e5322
Julie Blaye <jblaye@made-f0Br2s2.clusters.42paris.fr>
2024-10-22 14:51:05 +0200

    [ADD] example CGI script in Python

---
+++ cgi-bin/say_hello.py
@@ -0,0 +1,45 @@
+import cgi, os, datetime
+
+form = cgi.FieldStorage
+
+name = form.getvalue('name', 'wonderful person')
+
+print ("Content-type:text/html")
+
+response_body = f"""
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Hello you - WebSerVaBienOuBien Tester</title>
+		<link href="../web/style.css" rel="stylesheet">
+	</head>
+	<body>
+		<header>
+			<nav class="navbar">
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="about.html">About</a></li>
+					<li><a href="contact.html">Contact</a></li>
+					<li class="active"><a href="say_hello.html">Say Hello</a></li>
+				</ul>
+			</nav>
+		</header>
+		<main>
+			<h1>Hello {name}!</h1>
+			<p>You look great today!</p>
+		</main>
+	</body>
+</html>
+"""
+
+content_length = len(response_body)
+
+print(f"Content-Length: {content_length}")
+if "SERVER_NAME" in os.environ:
+	server_name = os.environ.get('SERVER_NAME')
+	print(f"Server: {server_name}")
+date = datetime.datetime.now().astimezone().strftime("%a, %d %b %Y %T %Z")
+print(f"Date: {date}\n")
+
+print(response_body)
