a49d1111438d95ea71d2ee80e3a8fd5fb87f5855
Hubert Vecchioli <hvecchio@made-f0Br3s4.clusters.42paris.fr>
2024-10-23 15:04:25 +0200

    [MOD] Corrected a segfault linked to the last client disconnection

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hvecchio <hvecchio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 16:23:30 by hvecchio          #+#    #+#             */
-/*   Updated: 2024/10/23 14:58:27 by hvecchio         ###   ########.fr       */
+/*   Updated: 2024/10/23 14:59:42 by hvecchio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ int main(int ac, char **av)
 			}
 		}
 		std::cout << "-------------------" << std::endl;
-		server.startServer(config); //Should I add the config object as an argument of initiate?
+		server.startServer(&config); //Should I add the config object as an argument of initiate?
 		server.runServer();
 	}
 	catch (std::exception & e)
