b97c6d3b591da7a98524a996392ff0a23ec51696
Hubert Vecchioli <hvecchio@made-f0Br7s12.clusters.42paris.fr>
2024-10-29 17:21:54 +0100

    [ADD] Added a folder with files to delete

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: hvecchio <hvecchio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 16:31:05 by hvecchio          #+#    #+#             */
-/*   Updated: 2024/10/29 16:36:19 by hvecchio         ###   ########.fr       */
+/*   Updated: 2024/10/29 16:40:26 by hvecchio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -237,6 +237,7 @@ void Server::_receiveRequest(int fd)
         }
         rawHTTPRequest[sizeHTTPRequest] = 0;
     }
+	std::cout<< rawHTTPRequest<<std::endl;
     HttpRequest* request = new HttpRequest(clientSendingARequest, rawHTTPRequest, sizeHTTPRequest);
     HttpResponse* response = new HttpResponse(*this, *request);
     request->setResponse(response);
