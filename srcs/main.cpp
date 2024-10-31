413c834c84339e5de2f05f6d4c51c39362331162
Julie Blaye <jublaye@hotmail.fr>
2024-10-31 10:56:27 +0100

    [MOD] added try catch to allow proper memory management

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,20 +2,26 @@
 
 int main(int ac, char **av)
 {
-	Server &server  = Server::getInstance();
-	signal(SIGINT, ft_stop_server);
-	try
-	{
-		ConfigurationFile config = ConfigurationFile(ac, av);
-		server.startServer(&config);
-		server.runServer();
+	try {
+		Server &server  = Server::getInstance();
+		signal(SIGINT, ft_stop_server);
+		try
+		{
+			ConfigurationFile config = ConfigurationFile(ac, av);
+			server.startServer(&config);
+			server.runServer();
+		}
+		catch (std::exception & e)
+		{
+			print(2, e.what());
+		}
+		server.cleanup();
+		delete &Server::getInstance();
+		print(1, "[Info] - Webserv is now stopped");
+		return (0);
 	}
-	catch (std::exception & e)
-	{
-		print(2, e.what());
+	catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+		return (1);
 	}
-	server.cleanup();
-	delete &Server::getInstance();
-	print(1, "[Info] - Webserv is now stopped");
-	return (0);
 }
